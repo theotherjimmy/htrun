eec9b9b0e2238e240dde918690de70ce0c851fb3
Conor Keegan <Conor.Keegan@arm.com>
2016-07-20 17:27:49 +0100

    Extracted the event loop into its own class
    Added a base class which represents the common elements of the event loop

---
+++ mbed_host_tests/host_tests_event_loop/base_event_loop.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+"""
+mbed SDK
+Copyright (c) 2011-2016 ARM Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from time import time
+from abc import ABCMeta, abstractmethod
+from mbed_host_tests.host_tests_logger import HtrunLogger
+
+class BaseEventLoop():
+    __metaclass__ = ABCMeta
+
+    def __init__(self, logger_name, event_queue, dut_event_queue, process, test_selector):
+        self.logger = HtrunLogger(logger_name)
+
+        self.event_queue = event_queue
+        self.dut_event_queue = dut_event_queue
+        self.callbacks = {}
+        self.p = process
+        self.test_selector = test_selector
+
+        self.result = None
+        self.start_time = None
+        # Default test case timeout
+        self.timeout_duration = 10
+
+    def register_callback(self, name, callback):
+        self.callbacks[name] = callback
+
+    def run_loop(self):
+        """! Run the event loop, which will process the events that are passed to
+             the event_queue, and possibly add events to the dut_event_queue.
+        @return TestSelection.TestResults.RESULT_* enum
+        """
+
+        self.start_time = time()
+        try:
+            while (time() - self.start_time) < self.timeout_duration:
+                # Handle default events like timeout, host_test_name, ...
+                if not self.event_queue.empty():
+                    try:
+                        event = self.event_queue.get(timeout=1)
+                    except QueueEmpty:
+                        continue
+                    continue_loop = self.process_event(event)
+                    if not continue_loop:
+                        break
+        except Exception:
+            self.logger.prn_err("something went wrong in the event loop!")
+            self.logger.prn_inf("==== Traceback start ====")
+            for line in traceback.format_exc().splitlines():
+                print line
+            self.logger.prn_inf("==== Traceback end ====")
+            self.result = test_selector.RESULT_ERROR
+
+        time_duration = time() - self.start_time
+        self.logger.prn_inf("test suite run finished after %.2f sec..."% time_duration)
+
+        self.run_finish()
+
+        return self.result
+
+    @abstractmethod
+    def process_event(self, event):
+        """! Process an indivdual event passed from the event_queue
+        @param event The event taken from the event_queue
+        @return True if the next event should be processed, or False if the 
+                event loop should stop
+        """
+        pass
+
+    @abstractmethod
+    def run_finish(self):
+        """! Perform any finishing processing after the main loop """
+        pass
