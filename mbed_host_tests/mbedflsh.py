f565b09d1463b04c70b7974fba8a46c7aa509c0e
Jimmy Brisson <theotherjimmy@gmail.com>
2017-11-14 15:30:54 -0600

    Parse mbedflsh arguments with argparse

--- mbed_host_tests/mbedflsh.py
+++ mbed_host_tests/mbedflsh.py
@@ -18,78 +18,67 @@ Author: Przemyslaw Wirkus <Przemyslaw.Wirkus@arm.com>
 """
 
 import sys
-import optparse
+import argparse
 
-### Flashing/Reset API provided by mbed--host-tests (mbedhtrun)
 from . import host_tests_plugins
 
 
-def cmd_parser_setup():
+def _cmd_parser_setup():
     """! Creates simple command line parser
     """
-    parser = optparse.OptionParser()
+    parser = argparse.ArgumentParser(
+        prog="mbedflsh",
+        description="Flash mbed devices from command line using mbed-host-test"
+                    " flashing plugins.")
+    commands = parser.add_argument_group(title="commands")\
+                     .add_mutually_exclusive_group(required=True)
 
-    parser.add_option('-f', '--file',
-                      dest='filename',
-                      help='File to flash onto mbed device')
+    commands.add_argument('-f', '--file',
+                          dest='filename',
+                          help='Flash a file onto an mbed device')
 
-    parser.add_option("-d", "--disk",
-                      dest="disk",
-                      help="Target disk (mount point) path. Example: F:, /mnt/MBED",
-                      metavar="DISK_PATH")
+    parser.add_argument("-d", "--disk",
+                        dest="disk",
+                        help="Target disk (mount point) path. Example: F:, /mnt/MBED",
+                        metavar="DISK_PATH")
 
-    copy_methods_str = "Plugin support: " + ', '.join(host_tests_plugins.get_plugin_caps('CopyMethod'))
+    parser.add_argument("-c", "--copy",
+                        dest="copy_method",
+                        default='shell',
+                        choices=host_tests_plugins.get_plugin_caps('CopyMethod'),
+                        help="Copy (flash the target) method selector.",
+                        metavar="COPY_METHOD")
 
-    parser.add_option("-c", "--copy",
-                      dest="copy_method",
-                      default='shell',
-                      help="Copy (flash the target) method selector. " + copy_methods_str,
-                      metavar="COPY_METHOD")
+    commands.add_argument('--plugins',
+                          dest='list_plugins',
+                          default=False,
+                          action="store_true",
+                          help='Print registered plugins and exit')
 
-    parser.add_option('', '--plugins',
-                      dest='list_plugins',
-                      default=False,
-                      action="store_true",
-                      help='Prints registered plugins and exits')
-
-    parser.add_option('', '--version',
-                      dest='version',
-                      default=False,
-                      action="store_true",
-                      help='Prints package version and exits')
-
-    parser.description = """Flash mbed devices from command line.""" \
-        """This module is using build in to mbed-host-tests plugins used for flashing mbed devices"""
-    parser.epilog = """Example: mbedflsh -d E: -f /path/to/file.bin"""
-
-    (opts, args) = parser.parse_args()
-    return (opts, args)
+    commands.add_argument('--version',
+                          dest='version',
+                          default=False,
+                          action="store_true",
+                          help='Print package version and exit')
+    return parser
 
 
 def main():
-    """! Function wrapping flashing (copy) plugin
-    @details USers can use mbedflsh command to flash mbeds from command line
+    """! The `mbedflsh` command flashes mbeds from command line
     """
-    errorlevel_flag = 0
-    (opts, args) = cmd_parser_setup()
+    parser = _cmd_parser_setup()
+    opts = parser.parse_args(sys.argv[1:])
 
     if opts.version:
-        import pkg_resources  # part of setuptools
+        import pkg_resources
         version = pkg_resources.require("mbed-host-tests")[0].version
         print(version)
         sys.exit(0)
-    elif opts.list_plugins:    # --plugins option
+    elif opts.list_plugins:
         host_tests_plugins.print_plugin_info()
         sys.exit(0)
-    else:
-        pass
-
-    if opts.filename:
-        print("mbedflsh: opening file %s..."% opts.filename)
-        result = host_tests_plugins.call_plugin('CopyMethod',
-            opts.copy_method,
-            image_path=opts.filename,
-            destination_disk=opts.disk)
-        errorlevel_flag = result == True
-
-    return errorlevel_flag
+    elif opts.filename:
+        print("mbedflsh: opening file %s..." % opts.filename)
+        sys.exit(host_tests_plugins.call_plugin(
+            'CopyMethod', opts.copy_method,
+            image_path=opts.filename, destination_disk=opts.disk))
